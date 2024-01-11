ef128d1a747a794809fe4d721f4f60294445f658
marouansw <marouansw@example.com>
2024-01-11 14:13:22 +0100

    commit3

--- travel_agency/urls.py
+++ travel_agency/urls.py
@@ -22,4 +22,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', include('pages.urls')),
     path('admin/', admin.site.urls),
+    path('', include('django.contrib.auth.urls')),
 ]
